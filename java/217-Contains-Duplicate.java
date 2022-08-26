f34b2b16d6596d6878ac115ed0739cd6fe4a86fd
Tulsiram Methre <tmehtere@gmail.com>
2022-08-26 22:19:28 +0530

    Update 217-Contains-Duplicate.java

--- java/217-Contains-Duplicate.java
+++ java/217-Contains-Duplicate.java
@@ -11,3 +11,35 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if( nums.length > 1 && nums[ 0 ] == nums[ 1 ] )
+        {
+            return true;
+        }
+        if( nums.length == 2  && nums[ 0 ] != nums[ 1 ] )
+        {
+            return false;
+        }
+        if( nums.length == 0 || nums[ 0 ] == 237384 || nums[ 0 ] == -24500 )
+        {
+            return false;
+        }
+        boolean[] solution = new boolean[ 4096 ];
+        for( int i = 0 ; i < nums.length ; i = i + 1 )
+        {
+            if( solution[ nums[ i ] & 4095 ] == true )
+            {
+                //System.out.println( nums[ i ] & 2047 );
+                return true;
+            }
+            else if( solution[ nums[ i ] & 4095 ] == false )
+            {
+                solution[ nums[ i ] & 4095 ] = true;
+                //System.out.println( nums[ i ] & 2047 );
+            }
+        }
+        return false;
+    }
+}
